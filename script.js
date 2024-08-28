77df764bb14ea372cc2c03db271d124bc8e5c18c
Nikki-boop <bogdnico@gmail>
2024-08-28 10:46:59 -0500

    homework1

---
+++ script.js
@@ -0,0 +1,31 @@
+console.log("Homework");
+
+
+//1.Declre variables
+// Using let
+
+let person = "Nicole";
+let action = "struggling";
+let subject = "homework";
+let place = " at home";
+let time = "now";
+
+// Using "const"
+const situation = "online school";
+
+// 2. Create the Story
+console.log("--- Funny Story ---");
+console.log(" One frantic night " + person + " was " + action + " with " + subject  +  place);
+console.log(" When all of a sudden " + person + " remembered " + situation);
+
+// 3. Experiment with Different Values
+console.log("-----Different version of the Story---");
+
+// Change values to create a different story
+person = "Nicole";
+action = "relaxing";
+subject = " a casual read ";
+place = " a beach ";
+time = " One Sunday ";
+
+console.log(time + person + " was " + action + " at" + place + " when she remembered " + situation);
